cf2f3f58017c568e425c5bf4f7a21fae730395fe
Pranay <X23201827@student.ncirl.ie>
2025-04-03 12:53:03 +0530

    Unit Test

--- auction_app/tests.py
+++ auction_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+#from django.contrib.auth.models import UserManager
+from django.urls import reverse
+from auction_app.models import UserModel
 
-# Create your tests here.
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "testpassword"
+        self.email = "testemail@gmail.com"
+        self.user = UserModel.objects.create_user(username=self.username,email=self.email, password=self.password)

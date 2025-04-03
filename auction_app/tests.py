c4b9fdc7a30a8cd008bc779c058911a2e9bb1b04
Pranay <X23201827@student.ncirl.ie>
2025-04-03 13:09:55 +0530

    changes for Sonar test

--- auction_app/tests.py
+++ auction_app/tests.py
@@ -9,3 +9,29 @@ class LoginTestCase(TestCase):
         self.password = "testpassword"
         self.email = "testemail@gmail.com"
         self.user = UserModel.objects.create_user(username=self.username,email=self.email, password=self.password)
+
+    """ def test_login_successful(self):
+        response = self.client.post(reverse('login'), {
+            'username': self.username,
+            'password': self.password
+        })
+        self.assertRedirects(response, reverse('home'))  
+
+    def test_login_invalid_credentials(self):   
+        response = self.client.post(reverse('login'), {
+            'username': "wronguser",
+            'password': "wrongpassword"
+        })
+        self.assertEqual(response.status_code, 200)  
+        self.assertContains(response, "Invalid username or password")
+
+    def test_login_blank_fields(self):
+        response = self.client.post(reverse('login'), {
+            'username': "",
+            'password': ""
+        })
+        self.assertEqual(response.status_code, 200)  
+
+    def test_login_get_request(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)  """
